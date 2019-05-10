c41273d77250296013595b27ccc081450667e1f1
lpusok <7979773+lpusok@users.noreply.github.com>
2019-05-10 15:06:19 +0200

    Modifying copy process for test addon (#115)
    * Modifying copy process for test addon
    
    * Escape test bundle name
    
    * Seprate result exports to seperate functions.
    
    * dep update
    
    * Use BitrisePerStepTestResultDirEnvKey from config
    
    * Adding check for test results addon output existence
    
    * dep update

---
+++ result_addon.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/bitrise-io/go-utils/command"
+	"github.com/bitrise-io/go-utils/log"
+)
+
+type addonCopy struct {
+	sourceTestOutputDir   string
+	targetAddonPath       string
+	targetAddonBundleName string
+}
+
+func copyAndSaveMetadata(info addonCopy) error {
+	info.targetAddonBundleName = replaceUnsupportedFilenameCharacters(info.targetAddonBundleName)
+	addonPerStepOutputDir := filepath.Join(info.targetAddonPath, info.targetAddonBundleName)
+
+	if err := copyDirectory(info.sourceTestOutputDir, addonPerStepOutputDir); err != nil {
+		return err
+	}
+	if err := saveBundleMetadata(addonPerStepOutputDir, info.targetAddonBundleName); err != nil {
+		return err
+	}
+	return nil
+}
+
+func copyDirectory(sourceBundle string, targetDir string) error {
+	if err := os.MkdirAll(targetDir, 0700); err != nil {
+		return fmt.Errorf("failed to create directory (%s), error: %s", targetDir, err)
+	}
+
+	// the leading `/` means to copy not the content but the whole dir
+	// -a means a better recursive, with symlinks handling and everything
+	cmd := command.New("cp", "-a", sourceBundle, targetDir+"/")
+	log.Donef("$ %s", cmd.PrintableCommandArgs())
+	if out, err := cmd.RunAndReturnTrimmedCombinedOutput(); err != nil {
+		return fmt.Errorf("copy failed, error: %s, output: %s", err, out)
+	}
+
+	return nil
+}
+
+func saveBundleMetadata(outputDir string, bundleName string) error {
+	// Save test bundle metadata
+	type testBundle struct {
+		BundleName string `json:"test-name"`
+	}
+	bytes, err := json.Marshal(testBundle{
+		BundleName: bundleName,
+	})
+	if err != nil {
+		return fmt.Errorf("could not encode metadata, error: %s", err)
+	}
+	if err = ioutil.WriteFile(filepath.Join(outputDir, "test-info.json"), bytes, 0600); err != nil {
+		return fmt.Errorf("failed to write file, error: %s", err)
+	}
+	return nil
+}
