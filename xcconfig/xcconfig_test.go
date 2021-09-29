4fa181e36f7aee2d376a731e55337f44600b86a2
Bence Zsigmond Nagy <nbzs1001@gmail.com>
2021-09-29 16:41:46 +0200

    Updated mock package names in tests

--- xcconfig/xcconfig_test.go
+++ xcconfig/xcconfig_test.go
@@ -1,8 +1,8 @@
 package xcconfig
 
 import (
-	fileUtilMocks "github.com/bitrise-io/go-utils/fileutil/mocks"
-	pathUtilMocks "github.com/bitrise-io/go-utils/pathutil/mocks"
+	mockfileutil "github.com/bitrise-io/go-utils/fileutil/mocks"
+	mockpathutil "github.com/bitrise-io/go-utils/pathutil/mocks"
 	"github.com/stretchr/testify/assert"
 	"io/fs"
 	"path/filepath"
@@ -14,9 +14,9 @@ func Test_WhenWritingXCConfigContent_ThenItShouldReturnFilePath(t *testing.T) {
 	testContent := "TEST"
 	testTempDir := "temp_dir"
 	expectedPath := filepath.Join(testTempDir, "temp.xcconfig")
-	mockPathProvider := new(pathUtilMocks.PathProvider)
+	mockPathProvider := new(mockpathutil.PathProvider)
 	mockPathProvider.On("CreateTempDir", "").Return(testTempDir, nil)
-	mockFileWriter := new(fileUtilMocks.FileWriter)
+	mockFileWriter := new(mockfileutil.FileWriter)
 	mockFileWriter.On("Write", expectedPath, testContent, fs.FileMode(0644)).Return(nil)
 	xcconfigWriter := NewWriter(mockPathProvider, mockFileWriter)
 
