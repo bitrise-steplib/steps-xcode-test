2f97fe3da37f20828e491c28f86ef7985808c40f
Szabolcs Toth <54896607+tothszabi@users.noreply.github.com>
2021-11-08 09:40:18 +0000

    Add step tests (#194)

--- step/step_test.go
+++ step/step_test.go
@@ -1,34 +1,44 @@
 package step
 
 import (
-	"testing"
-
-	mocklog "github.com/bitrise-io/go-utils/log/mocks"
+	"fmt"
+	"github.com/bitrise-io/go-steputils/stepconf"
+	mockenv "github.com/bitrise-io/go-utils/env/mocks"
+	"github.com/bitrise-io/go-utils/log"
 	mockPathutil "github.com/bitrise-io/go-utils/pathutil/mocks"
 	mockcache "github.com/bitrise-steplib/steps-xcode-test/cache/mocks"
+	mockexporter "github.com/bitrise-steplib/steps-xcode-test/output/mocks"
+	"github.com/bitrise-steplib/steps-xcode-test/simulator"
 	mocksimulator "github.com/bitrise-steplib/steps-xcode-test/simulator/mocks"
+	"github.com/bitrise-steplib/steps-xcode-test/xcodebuild"
 	mockxcodebuild "github.com/bitrise-steplib/steps-xcode-test/xcodebuild/mocks"
+	mockinstaller "github.com/bitrise-steplib/steps-xcode-test/xcpretty/mocks"
+	"github.com/hashicorp/go-version"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+	"strings"
+	"testing"
 )
 
-func Test_WhenTestRuns_ThenXcodebuildGetsCalled(t *testing.T) {
+type testingMocks struct {
+	installer *mockinstaller.Installer
+	xcodebuilder *mockxcodebuild.Xcodebuild
+	simulatorManager *mocksimulator.Manager
+	cache *mockcache.SwiftPackageCache
+	outputExporter *mockexporter.Exporter
+	pathModifier *mockPathutil.PathModifier
+	pathProvider *mockPathutil.PathProvider
+}
+
+func Test_GivenStep_WhenRuns_ThenXcodebuildGetsCalled(t *testing.T) {
 	// Given
-	logger := createLogger()
+	step, mocks := createStepAndMocks(nil)
 
-	xcodebuilder := new(mockxcodebuild.Xcodebuild)
-	xcodebuilder.On("RunTest", mock.Anything).Return("", 0, nil)
-
-	simulatorManager := new(mocksimulator.Manager)
-	simulatorManager.On("ResetLaunchServices").Return(nil)
-
-	cache := new(mockcache.SwiftPackageCache)
-	cache.On("SwiftPackagesPath", mock.Anything).Return("", nil)
-
-	pathProvider := new(mockPathutil.PathProvider)
-	pathProvider.On("CreateTempDir", mock.Anything).Return("tmp_dir", nil)
-
-	step := NewXcodeTestRunner(nil, logger, nil, xcodebuilder, simulatorManager, cache, nil, nil, pathProvider)
+	mocks.xcodebuilder.On("RunTest", mock.Anything).Return("", 0, nil)
+	mocks.simulatorManager.On("ResetLaunchServices").Return(nil)
+	mocks.cache.On("SwiftPackagesPath", mock.Anything).Return("", nil)
+	mocks.pathProvider.On("CreateTempDir", mock.Anything).Return("tmp_dir", nil)
 
 	config := Config{
 		ProjectPath: "./project.xcodeproj",
@@ -57,17 +67,222 @@ func Test_WhenTestRuns_ThenXcodebuildGetsCalled(t *testing.T) {
 
 	// Then
 	require.NoError(t, err)
-	xcodebuilder.AssertCalled(t, "RunTest", mock.Anything)
+	mocks.xcodebuilder.AssertCalled(t, "RunTest", mock.Anything)
 }
 
-func createLogger() (logger *mocklog.Logger) {
-	logger = new(mocklog.Logger)
-	logger.On("Infof", mock.Anything, mock.Anything).Return()
-	logger.On("Debugf", mock.Anything, mock.Anything).Return()
-	logger.On("Donef", mock.Anything, mock.Anything).Return()
-	logger.On("Printf", mock.Anything, mock.Anything).Return()
-	logger.On("Errorf", mock.Anything, mock.Anything).Return()
-	logger.On("Println").Return()
-	logger.On("EnableDebugLog", mock.Anything).Return()
-	return
+func Test_GivenXcode13OrNewer_WhenShouldRetryTestOnFailIsSet_ThenFails(t *testing.T) {
+	// Given
+	envValues := defaultEnvValues()
+	envValues["should_retry_test_on_fail"] = "yes"
+
+	step, mocks := createStepAndMocks(envValues)
+
+	ver := newVersion(13)
+	mocks.xcodebuilder.On("Version").Return(ver, nil)
+
+	// When
+	_, err := step.ProcessConfig()
+
+	// Then
+	require.Error(t, err)
+}
+
+func Test_GivenXcode12OrOlder_WhenTestRepetitionModeIsSet_ThenFails(t *testing.T) {
+	// Given
+	envValues := defaultEnvValues()
+	envValues["test_repetition_mode"] = "retry_on_failure"
+
+	step, mocks := createStepAndMocks(envValues)
+
+	ver := newVersion(12)
+	mocks.xcodebuilder.On("Version").Return(ver, nil)
+
+	// When
+	_, err := step.ProcessConfig()
+
+	// Then
+	require.Error(t, err)
+}
+
+func Test_GivenTestRepetitionModeIsNone_WhenRelaunchTestsForEachRepetitionIsSet_ThenFails(t *testing.T) {
+	// Given
+	envValues := defaultEnvValues()
+	envValues["relaunch_tests_for_each_repetition"] = "yes"
+
+	step, mocks := createStepAndMocks(envValues)
+
+	ver := newVersion(12)
+	mocks.xcodebuilder.On("Version").Return(ver, nil)
+
+	path := strings.TrimPrefix(envValues["project_path"], ".")
+	mocks.pathModifier.On("AbsPath", mock.Anything).Return(path, nil)
+
+	mocks.simulatorManager.On("GetLatestSimulatorAndVersion", mock.Anything, mock.Anything).Return(defaultSimulator(), "15", nil)
+
+	// When
+	_, err := step.ProcessConfig()
+
+	// Then
+	require.Error(t, err)
+}
+
+func Test_GivenStep_WhenInstallXcpretty_ThenInstallIt(t *testing.T) {
+	// Given
+	step, mocks := createStepAndMocks(nil)
+
+	ver, err := version.NewVersion("1.0")
+	if err != nil {
+		assert.Fail(t, fmt.Sprintf("%s", err))
+	}
+	mocks.installer.On("Install", mock.Anything).Return(ver, nil)
+
+	// When
+	err = step.InstallDeps(true)
+
+	// Then
+	assert.NoError(t, err)
+
+	mocks.installer.AssertCalled(t, "Install")
+}
+
+func Test_GivenStep_WhenExportsTestResult_ThenSetsCorrectly(t *testing.T) {
+	tests := []struct {
+		name string
+		testFailed bool
+	}{
+		{
+			name:       "Exports success status",
+			testFailed: false,
+		},
+		{
+			name:       "Exports failure status",
+			testFailed: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Log(test.name)
+
+		runExportTest(t,test.testFailed)
+	}
+}
+
+func runExportTest(t *testing.T, testFailed bool) {
+	// Given
+	step, mocks := createStepAndMocks(nil)
+
+	mocks.outputExporter.On("ExportTestRunResult", testFailed)
+
+	// When
+	err := step.Export(Result{}, testFailed)
+
+	// Then
+	assert.NoError(t, err)
+
+	mocks.outputExporter.AssertCalled(t, "ExportTestRunResult", testFailed)
+}
+
+func Test_GivenStep_WhenExport_ThenExportsAllTestArtifacts(t *testing.T) {
+	// Given
+	step, mocks := createStepAndMocks(nil)
+	result := defaultResult()
+	simulatorName := "test-name"
+
+	mocks.simulatorManager.On("SimulatorDiagnosticsName").Return(simulatorName, nil)
+	mocks.outputExporter.On("ExportTestRunResult", mock.Anything)
+	mocks.outputExporter.On("ExportXCResultBundle", result.DeployDir, result.XcresultPath, result.Scheme)
+	mocks.outputExporter.On("ExportXcodebuildBuildLog", result.DeployDir, result.XcodebuildBuildLog).Return(nil)
+	mocks.outputExporter.On("ExportXcodebuildTestLog", result.DeployDir, result.XcodebuildTestLog).Return(nil)
+	mocks.outputExporter.On("ExportSimulatorDiagnostics", result.DeployDir, result.SimulatorDiagnosticsPath, simulatorName).Return(nil)
+
+	// When
+	err := step.Export(result, false)
+
+	// Then
+	assert.NoError(t, err)
+
+	mocks.outputExporter.AssertCalled(t, "ExportXCResultBundle", result.DeployDir, result.XcresultPath, result.Scheme)
+	mocks.outputExporter.AssertCalled(t, "ExportXcodebuildBuildLog", result.DeployDir, result.XcodebuildBuildLog)
+	mocks.outputExporter.AssertCalled(t, "ExportXcodebuildTestLog", result.DeployDir, result.XcodebuildTestLog)
+	mocks.outputExporter.AssertCalled(t, "ExportSimulatorDiagnostics", result.DeployDir, result.SimulatorDiagnosticsPath, simulatorName)
+}
+
+// Helpers
+
+func defaultEnvValues() map[string]string {
+	return map[string]string{
+		"project_path": "./_tmp/BullsEye.xcworkspace",
+		"scheme": "BullsEye",
+		"destination": "platform=iOS Simulator,name=iPhone 8 Plus,OS=latest",
+		"test_repetition_mode": "none",
+		"maximum_test_repetitions": "3",
+		"relaunch_tests_for_each_repetition": "no",
+		"should_retry_test_on_fail": "no",
+		"perform_clean_action": "no",
+		"log_formatter": "xcpretty",
+		"cache_level": "swift_packages",
+		"verbose_log": "no",
+		"collect_simulator_diagnostics": "never",
+		"headless_mode": "yes",
+	}
+}
+
+func defaultSimulator() simulator.Simulator {
+	return simulator.Simulator{
+		Name:        "iPhone 8 Plus",
+		ID:          "E8C36A8B-543A-4477-BB91-699C0A9EA352",
+		Status:      "Shutdown",
+		StatusOther: "",
+	}
+}
+
+func defaultResult() Result {
+	return Result{
+		Scheme:                   "Scheme",
+		DeployDir:                "DeployDir",
+		XcresultPath:             "XcresultPath",
+		XcodebuildBuildLog:       "XcodebuildBuildLog",
+		XcodebuildTestLog:        "XcodebuildTestLog",
+		SimulatorDiagnosticsPath: "SimulatorDiagnosticsPath",
+	}
+}
+
+func createStepAndMocks(envValues map[string]string) (XcodeTestRunner, testingMocks) {
+	envRepository := new(mockenv.Repository)
+	call := envRepository.On("Get", mock.Anything)
+	call.RunFn = func(arguments mock.Arguments) {
+		key := arguments[0].(string)
+		value := envValues[key]
+		call.ReturnArguments = mock.Arguments{value, nil}
+	}
+
+	inputParser := stepconf.NewInputParser(envRepository)
+	installer := new(mockinstaller.Installer)
+	xcodebuilder := new(mockxcodebuild.Xcodebuild)
+	simulatorManager := new(mocksimulator.Manager)
+	cache := new(mockcache.SwiftPackageCache)
+	outputExporter := new(mockexporter.Exporter)
+	pathModifier := new(mockPathutil.PathModifier)
+	pathProvider := new(mockPathutil.PathProvider)
+
+	step := NewXcodeTestRunner(inputParser, log.NewLogger(), installer, xcodebuilder, simulatorManager, cache, outputExporter, pathModifier, pathProvider)
+	mocks := testingMocks{
+		installer: installer,
+		xcodebuilder: xcodebuilder,
+		simulatorManager: simulatorManager,
+		cache: cache,
+		outputExporter: outputExporter,
+		pathModifier: pathModifier,
+		pathProvider: pathProvider,
+	}
+
+	return step, mocks
+}
+
+func newVersion(major int64) xcodebuild.Version {
+	return xcodebuild.Version{
+		Version:      "test-version",
+		BuildVersion: "test-build",
+		MajorVersion: major,
+	}
 }
--- step/utils.go
+++ step/utils.go
@@ -2,10 +2,11 @@ package step
 
 import (
 	"fmt"
-
 	"github.com/bitrise-io/go-utils/colorstring"
 	"github.com/bitrise-io/go-utils/log"
 	"github.com/bitrise-io/go-utils/stringutil"
+	"github.com/bitrise-steplib/steps-xcode-test/simulator"
+	"github.com/bitrise-steplib/steps-xcode-test/xcodebuild"
 )
 
 func printLastLinesOfXcodebuildTestLog(rawXcodebuildOutput string, isRunSuccess bool) {
@@ -28,4 +29,63 @@ is available in the $BITRISE_XCODEBUILD_TEST_LOG_PATH environment variable.
 
 If you have the Deploy to Bitrise.io step (after this step),
 that will attach the file to your build as an artifact!`))
+}
+
+func createConfig(input Input, projectPath string, xcodeMajorVersion int, sim simulator.Simulator) Config {
+	return Config{
+		ProjectPath: projectPath,
+		Scheme:      input.Scheme,
+		TestPlan:    input.TestPlan,
+
+		SimulatorID:       sim.ID,
+		IsSimulatorBooted: sim.Status != simulatorShutdownState,
+
+		XcodeMajorVersion: xcodeMajorVersion,
+
+		TestRepetitionMode:            input.TestRepetitionMode,
+		MaximumTestRepetitions:        input.MaximumTestRepetitions,
+		RelaunchTestForEachRepetition: input.RelaunchTestsForEachRepetition,
+		RetryTestsOnFailure:           input.RetryTestsOnFailure,
+
+		XCConfigContent:    input.XCConfigContent,
+		PerformCleanAction: input.PerformCleanAction,
+		XcodebuildOptions:  input.XcodebuildOptions,
+
+		LogFormatter:    input.LogFormatter,
+		XcprettyOptions: input.XcprettyOptions,
+
+		CacheLevel: input.CacheLevel,
+
+		CollectSimulatorDiagnostics: exportCondition(input.CollectSimulatorDiagnostics),
+		HeadlessMode:                input.HeadlessMode,
+
+		DeployDir: input.DeployDir,
+	}
+}
+
+func createTestParams(cfg Config, xcresultPath, swiftPackagesPath string) xcodebuild.TestRunParams {
+	testParams := xcodebuild.TestParams{
+		ProjectPath:                    cfg.ProjectPath,
+		Scheme:                         cfg.Scheme,
+		Destination:                    fmt.Sprintf("id=%s", cfg.SimulatorID),
+		TestPlan:                       cfg.TestPlan,
+		TestOutputDir:                  xcresultPath,
+		TestRepetitionMode:             cfg.TestRepetitionMode,
+		MaximumTestRepetitions:         cfg.MaximumTestRepetitions,
+		RelaunchTestsForEachRepetition: cfg.RelaunchTestForEachRepetition,
+		XCConfigContent:                cfg.XCConfigContent,
+		PerformCleanAction:             cfg.PerformCleanAction,
+		RetryTestsOnFailure:            cfg.RetryTestsOnFailure,
+		AdditionalOptions:              cfg.XcodebuildOptions,
+	}
+
+	return xcodebuild.TestRunParams{
+		TestParams:                         testParams,
+		LogFormatter:                       cfg.LogFormatter,
+		XcprettyOptions:                    cfg.XcprettyOptions,
+		RetryOnTestRunnerError:             true,
+		RetryOnSwiftPackageResolutionError: true,
+		SwiftPackagesPath:                  swiftPackagesPath,
+		XcodeMajorVersion:                  cfg.XcodeMajorVersion,
+	}
 }
