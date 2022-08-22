6b7ca85b02ff46e15cf8e5776d2333e5cf16b809
Laszlo Pusok <laszlo.pusok@bitrise.io>
2022-08-22 13:50:52 +0200

    Merge xcpretty_helper into xcpretty_installer

--- xcodecommand/xcpretty_installer.go
+++ xcodecommand/xcpretty_installer.go
@@ -14,6 +14,18 @@ type xcprettyDependencyManager struct {
 	xcpretty xcprettyManager
 }
 
+type xcprettyManager interface {
+	isDepInstalled() (bool, error)
+	installDep() []command.Command
+	depVersion() (*version.Version, error)
+}
+
+type xcpretty struct {
+	commandFactory     command.Factory
+	rubyEnv            ruby.Environment
+	rubyCommandFactory ruby.CommandFactory
+}
+
 func NewXcprettyDependencyManager(logger log.Logger, commandFactory command.Factory, rubyCommandFactory ruby.CommandFactory, rubyEnv ruby.Environment) DependencyInstaller {
 	return &xcprettyDependencyManager{
 		logger: logger,
@@ -51,4 +63,24 @@ func (c *xcprettyDependencyManager) Install() (*version.Version, error) {
 	}
 
 	return xcprettyVersion, nil
+}
+
+func (c *xcpretty) isDepInstalled() (bool, error) {
+	return c.rubyEnv.IsGemInstalled("xcpretty", "")
+}
+
+func (c *xcpretty) installDep() []command.Command {
+	cmds := c.rubyCommandFactory.CreateGemInstall("xcpretty", "", false, false, nil)
+	return cmds
+}
+
+func (c *xcpretty) depVersion() (*version.Version, error) {
+	cmd := c.commandFactory.Create("xcpretty", []string{"--version"}, nil)
+
+	versionOut, err := cmd.RunAndReturnTrimmedCombinedOutput()
+	if err != nil {
+		return nil, err
+	}
+
+	return version.NewVersion(versionOut)
 }
